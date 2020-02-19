67864a51b6cbe075d65f4ad7a2ab4917dd5ee299
Sam Nicholls <samstudio8@gmail.com>
2020-02-19 10:05:36 +0000

    Alignment stats for reads [#15]

--- scripts/zymo/nickloman/summariseStats.R
+++ scripts/zymo/nickloman/summariseStats.R
@@ -1,56 +1,42 @@
-library(tidyverse)
+library(dplyr)
 library(gtools)
+library(readr)
 
 # from https://gist.github.com/shujishigenobu/1858458 by shujishigenobu
 N50 <- function(x) {
     x.sorted <- rev(sort(x))
-    return(x.sorted[cumsum(as.numeric(x.sorted)) >= sum(x.sorted)*0.5][1])
+    return(x.sorted[cumsum(x.sorted) >= sum(x.sorted)*0.5][1])
 }
 
 args=commandArgs(T)
 statsfn = args[1]
-metadata = args[2]
-prefix = args[3]
+tsv_path = args[2]
 
-st=read_tsv(statsfn)
+st=read.table(statsfn, sep="\t", head=T, row.names=NULL, na=c("-"))
+st$alen <- as.numeric(st$alen) # wtf is R
 
-abundance_metadata=read_tsv(paste0("../metadata/",metadata,".txt"), comment="#")
 
-overallstats = st %>%
-  summarise(n=n(),
-            bases=sum(ALen/1),
-            meanlen=mean(ALen),
-            medianlen=median(ALen),
-            maxlen=max(ALen),
-            N50=N50(ALen)) %>%
-  mutate(run=prefix) %>%
-  select(run, n, bases, meanlen, medianlen, N50, maxlen) %>%
-  write_tsv(paste0(prefix,"_run_stats.tsv"))
-
-a=st %>% group_by(Genome) %>% 
+a=st %>% group_by(refname) %>% 
          summarise(n=n(),
-                   bases=sum(ALen/1),
-                   meanlen=mean(ALen),
-                   medianlen=median(ALen),
-                   maxlen=max(ALen),
-                   N50=N50(ALen))
+                   bases=sum(alen),
+                   meanlen=mean(alen),
+                   medianlen=median(alen),
+                   maxlen=max(alen),
+                   N50=N50(alen))
 
-sources=read_tsv("../metadata/sources.txt", comment="#")
-b=inner_join(a, sources, by=c("Genome"="Abbrev"))
-b=inner_join(b, abundance_metadata, by=c("Genome"="Abbrev"))
+sources=read.table("../ref.cfg", comment="#", sep="\t", head=T, row.names=NULL, na=c("-"))
+b=inner_join(a, sources, by="refname")
 
 total=sum(b$bases)
-c=b %>% mutate(Sample = prefix) %>%
-        mutate(Cov = bases/GenomeSize) %>%
+c=b %>% mutate(Cov = bases/(size*1e6)) %>%
         mutate(Perc = bases/total * 100) %>%
-        mutate(FoldChange = foldchange(Perc, Expected)) %>%
+        mutate(FoldChange = foldchange(Perc, abundance)) %>%
         arrange(Perc)
    
-print(c)
-write_tsv(c %>% select(Genome, bases, meanlen, medianlen, N50, maxlen, Cov, Perc, Expected, FoldChange, CellType), paste0(prefix,"_stats.tsv"))
+write_tsv(c %>% select(refname, n, bases, meanlen, medianlen, N50, maxlen, Cov, Perc, abundance, FoldChange, celltype), tsv_path)
 
-print(c%>%select(Genome,bases,N50,Cov,Perc,Expected,FoldChange,CellType))
+#print(c%>%select(refname,bases,N50,Cov,Perc,abundance,FoldChange,celltype))
 
-p=ggplot(c, aes(x=Genome, y=FoldChange, fill=CellType)) + geom_bar(stat="identity")
-ggsave(paste0(prefix,"_","foldchange.pdf"), p)
+#p=ggplot(c, aes(x=Genome, y=FoldChange, fill=CellType)) + geom_bar(stat="identity")
+#ggsave(paste0(prefix,"_","foldchange.pdf"), p)
 
